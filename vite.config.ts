4ada189ba2973d5487fc9e396751e74192998139
lauraximenam2 <lauraximena.mm@outlook.com>
2025-06-02 08:39:24 -0500

    Configurar para despliegue en GitHub Pages y construir versión de producción en /docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.VITE_APP_BASE_URL || '/DIRI_sesion9/',
+
+    plugins: [react()],
+
+    build: {
+      outDir: 'docs',
+
+      emptyOutDir: true,
+    },
+
+  };
+});
